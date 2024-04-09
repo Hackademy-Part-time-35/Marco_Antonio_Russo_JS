1dd86201b8eb9e99a49fc3afff08d919736267cf
Marco Antonio Russo <ocra94@gmail.com>
2024-04-09 21:05:34 +0200

    corretto Traccia_2

--- Esercitazione_2024.04.09/Traccia_2/js/main.js
+++ Esercitazione_2024.04.09/Traccia_2/js/main.js
@@ -5,4 +5,4 @@ let resto_gatti = gatti % fila;
 let num_gatti_per_nuova_fila = fila - resto_gatti;
 
 
-console.log(`Ci sono ${num_file.toFixed(0)} file di gatti e ne mancano ${num_gatti_per_nuova_fila} per una nuova fila, con un avanzo di ${resto_gatti}`);
+console.log(`Ci sono ${parseInt(num_file)} file di gatti e ne mancano ${num_gatti_per_nuova_fila} per una nuova fila, con un avanzo di ${resto_gatti}`);
