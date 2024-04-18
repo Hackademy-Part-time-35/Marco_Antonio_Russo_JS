ed92035ccc70e4cc22ebdb99b577438c8a9877d7
Marco Antonio Russo <ocra94@gmail.com>
2024-04-18 20:07:05 +0200

    completata esercitazione

---
+++ Esercitazione_2024.04.18/Traccia_3/js/main.js
@@ -0,0 +1,10 @@
+let array = [600,450,760,200,1500,675,800];
+let iva_array=[];
+
+array.forEach((a) =>{
+    let acc = a*22/122;
+    iva_array.push(acc.toFixed(2));
+});
+
+console.log(iva_array);
+
