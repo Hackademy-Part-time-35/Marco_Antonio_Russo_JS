ac881bcae49a168386a5c8c092be13011b6bd907
Marco Antonio Russo <ocra94@gmail.com>
2024-05-01 13:55:22 +0200

    effettuati miglioramenti esperienza utente e CSS

--- Esercitazione_2024.04.30/Traccia_2/js/main.js
+++ Esercitazione_2024.04.30/Traccia_2/js/main.js
@@ -37,45 +37,52 @@ let agenda = {
     visualizzaContatti: function (){
         this.contatti.forEach((elem,index)=>{
 
+            // cattura elementi generati
             let par_contatto = document.createElement("p");
             let delete_button = document.createElement("button");
             let modify_button = document.createElement("button");
             let button_div = document.createElement("div")
 
+            // generazione righe con i contatti
             par_contatto.textContent = `${elem.name} - Telefono:  ${elem.tel}`;
             delete_button.textContent = `Elimina contatto`;
             modify_button.textContent = `Modifica contatto`;
 
-            par_contatto.classList.add("d-flex","justify-content-between","p-3","align-items-center","rounded","ps-5","custom-bg-primary")
+            // stilizzazione colore sfondo alternato per maggiore leggibilità
+            par_contatto.classList.add("d-flex","justify-content-between","p-3","align-items-center","rounded","ps-5","custom-bg-primary", "shadow")
                 if(index % 2 == 0){
                     par_contatto.classList.add("custom-bg-secondary")
                 }
 
+            // stilizzazione pulsanti laterali per ogni contatto
             modify_button.classList.add("btn","btn-warning","modify-button")
             delete_button.classList.add("btn","btn-danger","delete-button","ms-2")
             button_div.classList.add("d-flex","gap-2");
 
+            // inserimento tag generati
             elenco_contatti.appendChild(par_contatto);
             par_contatto.appendChild(button_div);
             button_div.appendChild(modify_button);
             button_div.appendChild(delete_button);
-
+            
+            // cattura pulsanti generati
             modifica_contatto_toggle = document.querySelectorAll(".modify-button")
             elimina_contatto = document.querySelectorAll(".delete-button");
-                
+            
+            // attesa click per chiamare metodo eliminaContatto
             elimina_contatto[index].addEventListener("click" , function(){
                 agenda.eliminaContatto(index);
             });
 
+            // attesa click per chiamare metodo modificaContatto
             modifica_contatto_toggle[index].addEventListener("click", function(){
         
-                
+                // controllo per impedire apertura di più modifica contatto contemporaneamente
                 if(controlla_modifica === 0){
+                    par_contatto.classList.add("border","border-5", "border-warning")
                     agenda.modificaContatto(index, button_div, modify_button);
                     controlla_modifica = 1;
-                }else{
-                    
-                }    
+                }  
             });
 
         });
@@ -116,35 +123,66 @@ let agenda = {
 
         // metodo per la creazione di due nuovi input per la modifica del contatto nella riga selezionata
     modificaContatto: function(index_contatto,button_div, modify_button){
-        let input_area = document.createElement("form");
+        // cattura elementi generati
+        let input_area_form = document.createElement("form");
+        let input_area_name = document.createElement("div");
+        let input_area_tel = document.createElement("div");
         let new_name = document.createElement("input");
+        let label_name = document.createElement("label");
         let new_tel = document.createElement("input");
+        let label_tel = document.createElement("label");
         let cancel = document.createElement("i");
         let controllo_cancel = 0;
         
-
+        // definizione tipo input
         new_name.type = "text";
         new_tel.type = "number";
 
-        button_div.insertBefore(input_area, modify_button);
-
+        // inserimento testo nei label e cambio nome pulsante
+        label_name.textContent = "Nome";
+        label_tel.textContent = "Telefono";
         modify_button.textContent = "Salva modifiche"
+        
+        // posizionamento tasto annulla e area input modifica contatto
+        button_div.insertBefore(input_area_form, modify_button);
+        button_div.insertBefore(cancel, input_area_form);
+        
 
-        input_area.appendChild(cancel);
-        input_area.appendChild(new_name);
-        input_area.appendChild(new_tel);
-
+        // posizionamento tag
+        input_area_form.appendChild(input_area_name);
+        input_area_form.appendChild(input_area_tel);
+        input_area_name.appendChild(new_name);
+        input_area_name.appendChild(label_name);
+        input_area_tel.appendChild(new_tel);
+        input_area_tel.appendChild(label_tel);
+        
+        // definizione dei placeholder
         new_name.setAttribute("placeholder","Nuovo nome");
         new_tel.setAttribute("placeholder","Nuovo telefono");
         
+        // collegamento fra label e input
+        new_name.setAttribute("id","floatingName");
+        label_name.setAttribute("for","floatingName")
 
+        new_tel.setAttribute("id","floatingTel");
+        label_tel.setAttribute("for","floatingTel")
+
+        // cattura e inserimento del valore del contatto selezionato per inserirlo a priori nell'input di modifica
+        new_name.value = this.contatti[index_contatto].name;
+        new_tel.value = this.contatti[index_contatto].tel;
+
+        // stilizzazione tasto annulla modifiche
         cancel.style.cursor = "pointer";
         cancel.classList.add("fa-solid","fa-close","close-modify");
         
-        input_area.classList.add("d-flex","gap-1");
+        // stilizzazione area input modifica contatto
+        input_area_name.classList.add("form-floating")
+        input_area_tel.classList.add("form-floating")
+        input_area_form.classList.add("d-flex","gap-1");
         new_name.classList.add("form-control","modifica_contatto_input");
         new_tel.classList.add("form-control","modifica_numero_contatto_input");
 
+        // cattura input modifica contatto e tasto annulla modifiche
         modifica_nome_contatto = document.querySelectorAll(".modifica_contatto_input");
         modifica_numero_contatto = document.querySelectorAll(".modifica_numero_contatto_input");
         cancel_cross = document.querySelector(".close-modify")
@@ -162,6 +200,7 @@ let agenda = {
 
         });
         
+        // controllo chiamata funzione di sovrascrittura se non viene premuto il tasto annulla modifiche
         if(controllo_cancel === 0){
             modifica_contatto_toggle[index_contatto].addEventListener("click", function(){
                 agenda.sovrascriviContatto(index_contatto, new_name, new_tel);
@@ -172,6 +211,7 @@ let agenda = {
         // metodo per la sovrascrittura del contatto ricevuto dal metodo modifica contatto con conseguente chiusura dei nuovi input
     sovrascriviContatto: function(index_contatto, new_name, new_tel){
 
+        // controllo per evitare di inserire contatti vuoti
         if(new_name.value != ""){
             this.contatti[index_contatto].name = new_name.value;
         }
