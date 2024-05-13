ffbc85cfe82e88ec8b2e317e09168572b09c40fb
Marco Antonio Russo <ocra94@gmail.com>
2024-05-13 23:47:56 +0200

    feat(cards)annunci = aggiunte carte da file JSON

--- Presto/resources/js/annunci.js
+++ Presto/resources/js/annunci.js
@@ -2,7 +2,7 @@
 let btn_filter = document.querySelector("#filter_btn");
 let filter_menu = document.querySelector(".filter_menu");
 let controllo_filtri = "closed";
-
+let ads_container = document.querySelector("#ads-container");
 
 btn_filter.addEventListener("click", () =>{
 
@@ -17,5 +17,35 @@ btn_filter.addEventListener("click", () =>{
             btn_filter.innerHTML = `<i class="fa-solid fa-sliders"></i> Filter`;
             controllo_filtri = "closed";
         }
-   
+
 });
+
+fetch("../../data/elenco_immobili.json")
+    .then(response => response.json())
+    .then(data =>{
+
+
+        function createCard(){
+            data.forEach(ads => {
+                let card = document.createElement("article");
+                card.classList.add("col-3","card-ads");
+                ads_container.appendChild(card);
+
+                card.innerHTML = `
+               
+                <img class="card-ads-img" src="../img/villa-hero-section-before.png" alt="">
+                <section class="card-ads-text">
+                    <h3>${ads.regione}</h3>
+                    <h4>${ads.prezzo} €</h4>
+                    <ul>
+                        <li>${ads.metri_quadrati} mq</li>
+                        <li>${ads.numero_camere} rooms</li>
+                    </ul>
+                </section>
+           
+                `
+
+            });
+        }
+        createCard();
+})
